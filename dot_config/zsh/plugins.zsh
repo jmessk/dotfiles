84d052c9572cfea2a1f49447d8c9cd8feca506a9
jmessk <dev@jmessk.net>
2026-03-11 17:16:17 +0900

    Add zsh

---
+++ dot_config/zsh/plugins.zsh
@@ -0,0 +1,12 @@
+export ZSH_PLUGINS=${DOT_ZSH}/plugins
+
+# Pure
+fpath+=(${ZSH_PLUGINS}/pure)
+autoload -U promptinit; promptinit
+prompt pure
+
+# zsh-syntax-highlighting
+source ${ZSH_PLUGINS}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+
+# zsh-autosuggestions
+source ${ZSH_PLUGINS}/zsh-autosuggestions/zsh-autosuggestions.zsh
